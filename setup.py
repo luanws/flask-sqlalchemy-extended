2906b864d44e10b0ff5e10ed64bb40d46dc22759
luanws <luan.w.silveira@gmail.com>
2020-11-16 17:19:42 -0300

    dependences

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",
+    name="flask-sqlalchemy-extended-luanws",
     version="0.0.1",
     author="luanws",
     author_email="luan.w.silveira@gmail.com",
-    description="A small example package",
+    description="SQLAlchemy autocomplete",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/luanws/flask-sqlalchemy-extended",
