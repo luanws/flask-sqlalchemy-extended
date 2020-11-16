f0516104379c568be51bc2f40718b4579448ce5f
luanws <luan.w.silveira@gmail.com>
2020-11-16 20:16:36 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flask-sqlalchemy-extended",
-    version="0.0.7",
+    version="0.0.8",
     author="luanws",
     author_email="luan.w.silveira@gmail.com",
     description="SQLAlchemy autocomplete",
