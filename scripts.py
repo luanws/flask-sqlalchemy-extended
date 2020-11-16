5fcbeb98957c91d5ec589b3cfb043c2596af4c76
luanws <luan.w.silveira@gmail.com>
2020-11-16 17:53:41 -0300

    Create scripts.py

---
+++ scripts.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+args = sys.argv[1:]
+
+scripts = [
+    ('build', 'python setup.py sdist bdist_wheel'),
+]
+
+commands = list(map(
+    lambda script: script[1],
+    filter(lambda script:  args.__contains__(script[0]), scripts)
+))
+
+[os.system(command) for command in commands]
