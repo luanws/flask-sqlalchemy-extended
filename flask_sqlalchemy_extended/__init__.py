7356886b5cec4efb8242d46978d1a1d7efab4421
luanws <luan.w.silveira@gmail.com>
2020-11-16 20:16:33 -0300

    Update __init__.py

--- flask_sqlalchemy_extended/__init__.py
+++ flask_sqlalchemy_extended/__init__.py
@@ -1,10 +1,13 @@
 from typing import Type
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.sqltypes import TypeEngine
 from .model import Model
 
 
-def Column(primary_key: bool):
-    pass
+def Column(
+    sqltype: TypeEngine,
+    primary_key: bool = False,
+): pass
 
 
 def create_all(bind='__all__', app=None):
